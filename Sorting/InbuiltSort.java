04e91d18cc6d866f1c8c4d1742a6a50f225585d5
snehachill <st957553@gmail.com>
2026-06-15 10:25:05 +0530

    feat: implement insertion sort and explore java inbuilt sorting methods

---
+++ Sorting/InbuiltSort.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class InbuiltSort {
+    public static void main(String[] args) {
+        int nums[]={5,4,3,2,1};
+        Arrays.sort(nums,0,3);
+        for(int num:nums){
+            System.out.print(num + " ");
+        }
+    }
+    }
