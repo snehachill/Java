fc8103ba033b78eddac0cb415bb93adac2adc384
snehachill <st957553@gmail.com>
2026-06-18 09:58:57 +0530

    feaat:Added a brute force diagonal sum for 2D array

---
+++ 2DArray/DiagonalSum.java
@@ -0,0 +1,31 @@
+public class DiagonalSum {
+
+    public static int DiagonalSum(int matrix[][]){
+        int sum=0;
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                //primary diagonal
+                if (i==j){
+                    sum += matrix[i][j];
+                }
+                //secondary diagonal
+                else if(i+j==n-1){
+                    sum += matrix[i][j];
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+        System.out.println(DiagonalSum(matrix));
+    }
+}
+
+
+
