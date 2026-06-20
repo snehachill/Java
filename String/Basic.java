34e3573cb5241ce6a23db37d7c0b8dbfceeddcf4
snehachill <st957553@gmail.com>
2026-06-20 10:33:30 +0530

    Basic programs of String

---
+++ String/Basic.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Basic {
+    public static void main(String[] args) {
+        /*String str="abc";
+        System.out.println(str);*/
+
+        Scanner sc=new Scanner(System.in);
+        String Name;
+        Name=sc.next();
+        System.out.println(Name);
+        System.out.println(Name.length());
+    }
+    
+}
---
+++ String/Concatenation.java
@@ -0,0 +1,8 @@
+public class Concatenation {
+    public static void main(String args[]){
+        String name="Sneha";
+        String lastname="Tiwari";
+        String fullname=name + " " +lastname;
+        System.out.println(fullname);
+    }
+}
