da32237094ab5356942f8705d29f33e557742db2
snehachill <st957553@gmail.com>
2026-06-24 14:05:21 +0530

    feat: implement basic bit operations (AND,OR,XOR...)

---
+++ Bit Manipulation/BitwiseOperator.java
@@ -0,0 +1,13 @@
+public class BitwiseOperator{
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println(a & b);//AND operator
+        System.out.println(a | b);//OR operator
+        System.out.println(a ^ b);//XOR operator
+        System.out.println(~ a);//1's compliment
+        System.out.println(5<<2);//left-shift
+        System.out.println(5>>2);//right-shift
+
+    }
+}
