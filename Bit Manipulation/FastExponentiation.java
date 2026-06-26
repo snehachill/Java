cf449e3835e37dd675e35c04103e73e1138f4e58
snehachill <st957553@gmail.com>
2026-06-26 12:21:36 +0530

    feat: implement O(log n) fast exponentiation using bitwise operators

---
+++ Bit Manipulation/FastExponentiation.java
@@ -0,0 +1,16 @@
+public class FastExponentiation {
+    public static int fastexpo(int a,int n) {
+        int ans=1;
+        while(n>0){
+            if((n & 1) !=0){//to check lsb
+               ans=ans*a;
+            }
+            a=a*a;
+            n=n>>1;
+        }
+    return ans ;   
+    }
+    public static void main(String[] args) {
+        System.out.println(fastexpo(3,5));
+    }
+}
