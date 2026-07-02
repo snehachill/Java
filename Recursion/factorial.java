654ae0303af88df199846aac45d7c79a4f9e65d9
snehachill <st957553@gmail.com>
2026-07-02 17:23:40 +0530

    feat: implement recursive factorial function with base case handling

---
+++ Recursion/factorial.java
@@ -0,0 +1,12 @@
+public class factorial {
+    public static int fact(int n){
+        if(n==0){
+            return 1;
+        }
+        int fact=n*fact(n-1);
+        return fact;
+    }
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+}
