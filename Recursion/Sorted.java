fc9db04e99708c32aceca021aaf14184d40f4394
snehachill <st957553@gmail.com>
2026-07-02 18:08:28 +0530

    feat: implement recursive array sorted check

---
+++ Recursion/Sorted.java
@@ -0,0 +1,16 @@
+public class Sorted {
+    public static boolean IsSorted(int arr[],int i) {
+        if(i==arr.length-1){
+            return true;
+        }
+        if(arr[i]>arr[i+1]){
+           return false;
+        }
+        return IsSorted(arr, i+1);
+
+    }
+    public static void main(String[] args) {
+        int arr[]={1,2,6,4,4};
+        System.out.println(IsSorted(arr, 0));
+    }
+}
