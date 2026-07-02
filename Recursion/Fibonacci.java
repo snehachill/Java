438ea23a698b835ea912e3f61401fe6964e0acae
snehachill <st957553@gmail.com>
2026-07-02 17:49:24 +0530

    feat: implement recursive fibonacci function

---
+++ Recursion/Fibonacci.java
@@ -0,0 +1,14 @@
+public class Fibonacci {
+    public static int fibSeries(int n) {
+        if(n==0 || n==1){
+            return n;
+        }
+        int fb1=fibSeries(n-1);
+        int fb2=fibSeries(n-2);
+        int fb=fb1+fb2;
+        return fb;
+    }
+    public static void main(String[] args) {
+        System.out.print(fibSeries(10));
+    }
+}
