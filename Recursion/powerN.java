c6af09c4aae4c9fbfaa925b9644076785b9b6baa
snehachill <st957553@gmail.com>
2026-07-03 10:34:25 +0530

    feat: implement recursive last occurrence search via reverse array traversal

---
+++ Recursion/powerN.java
@@ -0,0 +1,11 @@
+public class powerN {
+    public static int XpowerN(int x,int n){
+        if(n==0){
+            return 1;
+        }
+        return x* XpowerN(x, n-1);
+    }
+    public static void main(String[] args) {
+        System.out.println(XpowerN(2, 10));
+    }
+}
