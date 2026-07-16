bfb706f169f13dd5fa2d1145195c7910ecf1fd17
snehachill <st957553@gmail.com>
2026-07-16 11:17:30 +0530

    feat:Addeed last node in linked list

---
+++ LinkedList.java/Lastnode.java
@@ -0,0 +1,44 @@
+public class Lastnode {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    public void Addlast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data);
+            if (temp.next != null) {
+                System.out.print("->");
+            }
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Lastnode list = new Lastnode();
+        list.Addlast(10);
+        list.Addlast(20);
+        list.Addlast(30);
+        list.print();
+    }
+}
