1d1b6ca0f0883e4338c3b3422378d25246423ffa
snehachill <st957553@gmail.com>
2026-07-23 11:25:31 +0530

    feat:Finding the intersecting of two singly linked list

---
+++ Exercise/Linkedlist/IntersectionoftoLL.java
@@ -0,0 +1,50 @@
+public class IntersectionoftoLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public Node intersectingP(Node head1, Node head2) {
+        while (head2 != null) {
+            Node temp = head1;
+            while (temp != null) {
+                if (temp == head2) {
+                    return head2;
+                }
+                temp = temp.next;
+            }
+            head2 = head2.next;
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        IntersectionoftoLL list=new IntersectionoftoLL();
+        Node head1, head2;
+        head1 = new Node(10);
+        head2 = new Node(3);
+        Node newNode = new Node(6);
+        head2.next = newNode;
+        newNode = new Node(9);
+        head2.next.next = newNode;
+        newNode = new Node(15);
+        head1.next = newNode;
+        head2.next.next.next = newNode;
+        newNode = new Node(30);
+        head1.next.next = newNode;
+        head1.next.next.next = null;
+        Node IntersectionPoint=list.intersectingP(head1,head2);
+        if(IntersectionPoint==null){
+            System.out.print(" No Intersection Point \n");
+        }
+        else{
+            System.out.println("Intersecting point found : " + IntersectionPoint.data);
+        }
+}}
+
+
