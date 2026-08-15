f34746376d1d835fd81a92867a7c6521ca189365
snehachill <st957553@gmail.com>
2026-08-15 21:08:53 +0530

    feat: solve activity selection problem using greedy approach

---
+++ GreedyAlgo/ActivitySelection.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ActivitySelection {
+    public static void main(String[] args) {
+        int[] start = {1, 3, 0, 5, 8, 5};
+        int[] finish = {2, 4, 6, 7, 9, 9};
+       
+        int Max=0;
+        ArrayList<Integer>ans=new ArrayList<>();
+
+        Max=1;
+        ans.add(0);
+        int LastEnd=finish[0];
+
+        for(int i=0;i<finish.length;i++){
+            if(start[i]>=LastEnd){
+                Max++;
+                ans.add(i);
+                LastEnd=finish[i];
+            }
+        }
+        System.out.println("Max Activities: "+ Max);
+        for(int i=0;i<ans.size();i++){
+            System.out.println("A"+ans.get(i)+" ");
+        }
+    }
+} 
